88d18f6f87a1f1e0f7bed0db9f307696ab2fe04d
Jeffrey Varner <jvarner@pooksoft.com>
2021-03-27 07:29:02 -0400

    Upgrading to 1.6 and docs
    Adding documentation and upgrade to 1.6

--- docs/main.jl
+++ docs/main.jl
@@ -18,3 +18,12 @@ makedocs(sitename="PooksoftAlphaVantageDataStore.jl",
         ]
     ]
 )
+
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
+deploydocs(
+    repo = "github.com/Pooksoft/PooksoftAlphaVantageDataStore.jl.git",
+    devbranch = "main",
+    devurl = "dev",
+)
